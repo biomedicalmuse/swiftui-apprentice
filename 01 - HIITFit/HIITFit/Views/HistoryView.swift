e7815a20401c9eb491213393930e30f713b4e8ae
Natasha Godwin <natashasgodwin@gmail.com>
2021-07-11 14:26:34 -0400

    Built basic HistoryView UI

---
+++ 01 - HIITFit/HIITFit/Views/HistoryView.swift
@@ -0,0 +1,37 @@
+
+import SwiftUI
+
+struct HistoryView: View {
+    let today = Date()
+    let yesterday = Date().addingTimeInterval(-86400)
+    
+    let exercises1 = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+    let exercise2 = ["Squat", "Step Up", "Burpee"]
+    
+    var body: some View {
+        VStack {
+            Text("History")
+                .font(.title)
+                .padding()
+            Form {
+                Section(
+                    header: Text(today.formatted(as: "MMM d"))
+                        .font(.headline)) {
+                    
+                }
+                Section(
+                    header: Text(yesterday.formatted(as: "MMM d"))
+                        .font(.headline)) {
+                    
+                }
+            }
+        }
+            
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+    }
+}
