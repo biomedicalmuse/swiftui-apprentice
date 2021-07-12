7ecb7c626cf499ec514cae5838c8c53c81d4bbd5
Natasha Godwin <natashasgodwin@gmail.com>
2021-07-12 14:46:09 -0400

    Added data model, dev data, compiler directive

---
+++ 01 - HIITFit/HIITFit/Model/HistoryStore.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+struct ExerciseDay: Identifiable {
+	let id = UUID()
+	let date: Date
+	var exercises: [String] = []
+}
+
+struct HistoryStore {
+	var exerciseDays: [ExerciseDay] = []
+	
+	init() {
+		/*
+		#if DEBUG is a compiler directive
+		It's used to prevent dummy data from being included in the release version of the app.
+		It checks whether the current Build Configuration is Debug.
+		*/
+		#if DEBUG
+		createDevData()
+		#endif
+	}
+}
+
