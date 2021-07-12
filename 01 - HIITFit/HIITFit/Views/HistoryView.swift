3b2d7124c81383afd23a53fc6ea8dff0c3e71e70
Natasha Godwin <natashasgodwin@gmail.com>
2021-07-12 14:52:51 -0400

    Refactored HistoryView to use HistoryStore

--- 01 - HIITFit/HIITFit/Views/HistoryView.swift
+++ 01 - HIITFit/HIITFit/Views/HistoryView.swift
@@ -1,49 +1,37 @@
-
 import SwiftUI
 
 struct HistoryView: View {
-    let today = Date()
-    let yesterday = Date().addingTimeInterval(-86400)
-    
-    let exercises1 = ["Squat", "Step Up", "Burpee", "Sun Salute"]
-    let exercises2 = ["Squat", "Step Up", "Burpee"]
-    
-    var body: some View {
-        ZStack(alignment: .topTrailing) {
-            VStack {
-                Text("History")
-                    .font(.title)
-                    .padding()
-                Form {
-                    Section(
-                        header: Text(today.formatted(as: "MMM d"))
-                            .font(.headline)) {
-                        ForEach(exercises1, id: \.self) { exercise in
-                            Text(exercise)
-                        }
-                    }
-                    Section(
-                        header: Text(yesterday.formatted(as: "MMM d"))
-                            .font(.headline)) {
-                        ForEach(exercises2, id: \.self) { exercise in
-                            Text(exercise)
-                        }
-                    }
-                }
-            }
-            Button(action: {}) {
-                Image(systemName: "xmark.circle")
-                    .font(.title)
-                    .padding(.trailing)
-            }
-        }
-            
-    }
+	let history = HistoryStore()
+	 
+	 var body: some View {
+		  ZStack(alignment: .topTrailing) {
+				VStack {
+					 Text("History")
+						  .font(.title)
+						  .padding()
+					 Form {
+						ForEach(history.exerciseDays) { day in
+							Section(header: Text(day.date.formatted(as: "MMM d")).font(.headline)) {
+								ForEach(day.exercises, id: \.self) { exercise in
+									Text(exercise)
+								}
+							}
+						}
+					 }
+				}
+				Button(action: {}) {
+					 Image(systemName: "xmark.circle")
+						  .font(.title)
+						  .padding(.trailing)
+				}
+		  }
+				
+	 }
 }
 
 struct HistoryView_Previews: PreviewProvider {
-    static var previews: some View {
-        HistoryView()
-    }
+	 static var previews: some View {
+		  HistoryView()
+	 }
 }
 
