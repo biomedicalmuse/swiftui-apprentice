a6d1b328541a360edceeecc9acbc87155f969c81
Natasha Godwin <natashasgodwin@gmail.com>
2021-07-14 14:56:25 -0400

    Made it possible to add ratings

--- 01 - HIITFit/HIITFit/Views/ExerciseView.swift
+++ 01 - HIITFit/HIITFit/Views/ExerciseView.swift
@@ -3,6 +3,7 @@ import AVKit
 
 struct ExerciseView: View {
 	@Binding var selectedTab: Int
+	@State private var rating = 0
     let index: Int
     let interval: TimeInterval = 30
 	
@@ -37,7 +38,7 @@ struct ExerciseView: View {
 					}
 					  .font(.title3)
 					  .padding()
-                RatingView()
+					RatingView(rating: $rating)
                     .padding()
                 Spacer()
                 Button(NSLocalizedString("History", comment: "view user activity")) {}
